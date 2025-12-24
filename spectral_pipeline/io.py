65be128825c1a7b3dfa8080a9b6db68a67be5e1b
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-12-24 03:48:02 +0300

    Update io.py

--- spectral_pipeline/io.py
+++ spectral_pipeline/io.py
@@ -51,10 +51,10 @@ def load_records(root: Path) -> List[DataSet]:
             x, t, s = x[:end], t[:end], s[:end]
 
         # Для HF дополнительно ограничиваем длительность 0.08 нс
-        # if tag == "HF":
-        #     cutoff = 0.1e-9
-        #     end = np.searchsorted(t, cutoff, "right")
-        #     x, t, s = x[:end], t[:end], s[:end]
+        if tag == "HF":
+            cutoff = 0.08e-9
+            end = np.searchsorted(t, cutoff, "right")
+            x, t, s = x[:end], t[:end], s[:end]
 
         # Вырезаем выбросы
         s = np.where((136.9 < x) & (x < 137.05), s[np.where(x>=137.05)][0], s)
