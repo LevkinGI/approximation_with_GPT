c1bd76e4887354639b29ec32f8643349080d4bca
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-12-27 21:25:05 +0300

    Update io.py

--- spectral_pipeline/io.py
+++ spectral_pipeline/io.py
@@ -88,10 +88,10 @@ def load_records(root: Path) -> List[DataSet]:
         s = s[st:]
 
         # Для LF дополнительно ограничиваем длительность 0.7 нс
-        if tag == "LF":
-            cutoff = 0.3e-9
-            end = np.searchsorted(t, cutoff, "right")
-            t, s = t[:end], s[:end]
+        # if tag == "LF":
+        #     cutoff = 0.3e-9
+        #     end = np.searchsorted(t, cutoff, "right")
+        #     t, s = t[:end], s[:end]
 
         # Для HF дополнительно ограничиваем длительность 0.08 нс
         # if tag == "HF":
